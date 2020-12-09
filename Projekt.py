8a0fc22bf511a975d6fd1a86734a7e0d039e31f4
DusanStoiljkovic <dusanstoiljkovic02@gmail.com>
2020-12-09 18:15:28 +0100

    change line to row

--- Projekt.py
+++ Projekt.py
@@ -1,13 +1,13 @@
 import random
 
 def gensachovnica(n):  # odlucio sam se da cu ovu tablu uraditi preko 2D liste 
-    line = n         # redovi
+    row = n         # redovi
     column = n      # kolone 
 
     game_board = []          
     for i in range(column):
         column = []
-        for i in range(line):
+        for i in range(row):
             column.insert(i," ")
         game_board.append(column)
 
@@ -49,23 +49,23 @@ def prve_kolo(n,area):
     empty_place = round((n-3)/2)
     x = empty_place
     
-    line = 0
+    row = 0
     column = 0
     
     while True:     # toto dava panacika "A" do startnej pozicie ked spadne number6
         number = random.randint(1,6)
         print(number)
-        current_position_line = 0
+        current_position_row = 0
         current_position_column = 0
         
         if number == 6:
             board[0][x+2] = "A"
-            current_position_line = 0
+            current_position_row = 0
             current_position_column = x+2
 
             draw_board(board)
                     
-            return board, current_position_line, current_position_column
+            return board, current_position_row, current_position_column
             
         else:
             continue
@@ -73,15 +73,15 @@ def prve_kolo(n,area):
 
 
 # pohyb panacika A
-def pohyb_hraca_1(area, n, position_line, position_column):            # tu mi nastaje problem jer neznam da napravim algoritam
+def pohyb_hraca_1(area, n, position_row, position_column):            # tu mi nastaje problem jer neznam da napravim algoritam
     board = area
     players_per_team = round((n-3)/2)
     x = players_per_team
-    forward_position_line = position_line
+    forward_position_row = position_row
     forward_position_column = position_column
-    print("line", forward_position_line,"column",forward_position_column)
+    print("row", forward_position_row,"column",forward_position_column)
 
-    current_position_line = 0
+    current_position_row = 0
     current_position_column = 0
     
     number = int(input())    # tu si viberam number z kocky
@@ -90,58 +90,58 @@ def pohyb_hraca_1(area, n, position_line, position_column):            # tu mi n
     if forward_position_column > n//2:                  ## TOTO DOLE JE PEKLO 
         if forward_position_column == (n//2)+1:
             if number<= players_per_team:
-                if forward_position_line == players_per_team:
+                if forward_position_row == players_per_team:
                     board[players_per_team][forward_position_column+abs(number- players_per_team)] = "A"
-                    board[forward_position_line][forward_position_column] = "*"
-                    current_position_line = players_per_team
+                    board[forward_position_row][forward_position_column] = "*"
+                    current_position_row = players_per_team
                     current_position_column = forward_position_column+(number- players_per_team)
 
-                    return board, current_position_line, current_position_column
+                    return board, current_position_row, current_position_column
 
                 elif number<= players_per_team:
-                    if forward_position_line == players_per_team:
+                    if forward_position_row == players_per_team:
                         board[players_per_team][forward_position_column+(number- players_per_team)] = "A"
-                        board[forward_position_line][forward_position_column] = "*"
+                        board[forward_position_row][forward_position_column] = "*"
                         
                     else:
                         current_position_column = forward_position_column+(number- players_per_team)
-                        board[forward_position_line+number][forward_position_column] = "A"
-                        board[forward_position_line][forward_position_column] = "*"
-##                        current_position_line = forward_position_line+cislo
+                        board[forward_position_row+number][forward_position_column] = "A"
+                        board[forward_position_row][forward_position_column] = "*"
+##                        current_position_row = forward_position_row+cislo
 ##                        current_position_column = forward_position_column
 
-                    return board, current_position_line, current_position_column                
+                    return board, current_position_row, current_position_column                
                 elif number> players_per_team:
                     board[players_per_team][forward_position_column+(number- players_per_team)] = "A"
-                    board[forward_position_line][forward_position_column] = "*"
-                    current_position_line = players_per_team
+                    board[forward_position_row][forward_position_column] = "*"
+                    current_position_row = players_per_team
                     current_position_column = forward_position_column+(number- players_per_team)
 
-                    return board, current_position_line, current_position_column
+                    return board, current_position_row, current_position_column
 
             elif number> players_per_team:
                     board[players_per_team][forward_position_column+(number- players_per_team)] = "A"
-                    board[forward_position_line][forward_position_column] = "*"
-                    current_position_line = players_per_team
+                    board[forward_position_row][forward_position_column] = "*"
+                    current_position_row = players_per_team
                     current_position_column = forward_position_column+(number- players_per_team)
 
-                    return board, current_position_line, current_position_column
+                    return board, current_position_row, current_position_column
         
             
-##    if cislo+forward_position_line <= players_per_team:
-##        board[forward_position_line+cislo][forward_position_column] = "A"
-##        board[forward_position_line][forward_position_column] = "*"
-##        current_position_line = forward_position_line+cislo
+##    if cislo+forward_position_row <= players_per_team:
+##        board[forward_position_row+cislo][forward_position_column] = "A"
+##        board[forward_position_row][forward_position_column] = "*"
+##        current_position_row = forward_position_row+cislo
 ##        aktualna_pozicia_stlpcu = forward_position_column
 ##
-##    elif cislo+forward_position_line >= players_per_team:
-##        board[players_per_team][forward_position_line+2+cislo] = "A"
-##        board[forward_position_line][forward_position_column] = "*"
-##        current_position_line = players_per_team
-##        aktualna_pozicia_stlpcu = forward_position_line+2+cislo
+##    elif cislo+forward_position_row >= players_per_team:
+##        board[players_per_team][forward_position_row+2+cislo] = "A"
+##        board[forward_position_row][forward_position_column] = "*"
+##        current_position_row = players_per_team
+##        aktualna_pozicia_stlpcu = forward_position_row+2+cislo
         
     
-##    return board, current_position_line, aktualna_pozicia_stlpcu
+##    return board, current_position_row, aktualna_pozicia_stlpcu
 
     
 
@@ -189,15 +189,15 @@ def prever_n():         # Ovde sam odlucio da napisem funkciju koja ce proveriti
         return n
 
 
-def main_2(area,n,position_line,position_column):       # Tu se idigrava sve za  2deo projekta (isao sa na ideju da pamti krosle kordinate figure A)
-    new_area, new_position_line, new_position_column = pohyb_hraca_1(area,n,position_line, position_column)
+def main_2(area,n,position_row,position_column):       # Tu se idigrava sve za  2deo projekta (isao sa na ideju da pamti krosle kordinate figure A)
+    new_area, new_position_row, new_position_column = pohyb_hraca_1(area,n,position_row, position_column)
     draw_board(new_area)
     board = new_area
-    position_line = new_position_line
+    position_row = new_position_row
     position_column = new_position_column
-    print("Print z main2 :"+"rad",position_line,"stlpec",position_column)
+    print("Print z main2 :"+"rad",position_row,"stlpec",position_column)
 
-    main_2(area,n, position_line, position_column)
+    main_2(area,n, position_row, position_column)
 
 
 
@@ -205,5 +205,5 @@ def main_2(area,n,position_line,position_column):       # Tu se idigrava sve za 
 n = prever_n()
 area = gensachovnica(n)
 draw_board(area)
-board, position_line, position_column =  prve_kolo(n,area)
-main_2(area,n,position_line, position_column)
+board, position_row, position_column =  prve_kolo(n,area)
+main_2(area,n,position_row, position_column)
